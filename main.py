3cfa66b8a3c39de41479d9efa04074965473a55c
JRodriguezGonzalez <140106749+JRodriguezGonzalez@users.noreply.github.com>
2024-11-11 21:03:54 -0600

    commit

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
+from auth import router as auth_router  # Importa el enrutador de autenticación desde auth.py
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+load_dotenv()
+
+# Configura la sesión para la autenticación
+app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
+
+# Incluye el enrutador de autenticación en la aplicación principal
+app.include_router(auth_router)
